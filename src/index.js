628ca451af363aaa546a092653e542b6b4031b42
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-27 11:36:33 +0300

    html hard coded main-menu block

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import pageLoadfunction from "./pageLoad";
+// import pageLoadfunction from "./pageLoad";
 
-const content = document.querySelector('#content')
+// const content = document.querySelector('#content')
 
-const navigation = document.createElement('nav')
-navigation.innerHTML = ` <ul class="navigation">
-<div class="nav__link"><a href="">HOMEPAGE</a></div>
-<div class="nav__link"><a href="">MENU</a></div>
-<div class="nav__link"><a href="">HISTORY</a></div>
-</ul>`;
+// const navigation = document.createElement('nav')
+// navigation.innerHTML = `<ul class="navigation">
+// <div class="nav__link"><a href="">HOMEPAGE</a></div>
+// <div class="nav__link"><a href="">MENU</a></div>
+// <div class="nav__link"><a href="">HISTORY</a></div>
+// </ul>`;
 
-const mainContent = document.createElement('main')
-mainContent.innerHTML = `<h1>MEAT LINE</h1>
-<p class="home__description">Some text about how awesome this restaurant is</p>
-<button type="button">Go to menu</button>`
+// const mainContent = document.createElement('main')
+// mainContent.innerHTML = `<h1>MEAT LINE</h1>
+// <p class="home__description">Some text about how awesome this restaurant is</p>
+// <button type="button">Go to menu</button>`
 
-const footer = document.createElement('footer')
-footer.innerHTML = `<footer>Made by VoronovichDev</footer>`
+// const footer = document.createElement('footer')
+// footer.innerText = `Made by VoronovichDev`
 
-window.onload = () => {
-   pageLoadfunction(content, navigation, mainContent, footer)
-}
+// window.onload = () => {
+//    pageLoadfunction(content, navigation, mainContent, footer)
+// }
